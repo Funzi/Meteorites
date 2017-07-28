a13e9faa669fb80056ad15944f8dbf0e9fad3777
David Pribula <davidpribula@gmail.com>
2017-07-28 18:19:22 +0200

    Adding menu options

--- Meteorites/app/src/main/java/cz/pribula/meteorites/MainActivity.java
+++ Meteorites/app/src/main/java/cz/pribula/meteorites/MainActivity.java
@@ -4,20 +4,25 @@ import android.app.Fragment;
 import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.Button;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
-    Button button;
 import io.realm.Realm;
 import io.realm.RealmConfiguration;
+
+public class MainActivity extends AppCompatActivity  {
+
+    private static final String FRAGMENT_METEORITE_TAG = "fragment_meteorite";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Fragment frag = new MeteoriteFragment().newInstance("", "");
         FragmentTransaction ft = getFragmentManager().beginTransaction();
-        ft.add(R.id.fragment_container, frag, "").commit();
+        ft.add(R.id.fragment_container, frag, FRAGMENT_METEORITE_TAG).commit();
         setContentView(R.layout.activity_main);
+
         RealmConfiguration realmConfiguration = new RealmConfiguration.Builder(this)
                 .name(Realm.DEFAULT_REALM_NAME)
                 .schemaVersion(0)
@@ -25,4 +30,23 @@ import io.realm.RealmConfiguration;
                 .build();
         Realm.setDefaultConfiguration(realmConfiguration);
     }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_item_update: ((MeteoriteFragment) getFragmentManager().findFragmentByTag(FRAGMENT_METEORITE_TAG)).updateMeteorites();
+                                        return true;
+            case R.id.menu_item_about: return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
 }
